c9d2df864d95110667d470f958d3b5fd14da1261
Mihai <mihai8804858@gmail.com>
2015-05-16 22:19:26 -0700

    Message Inbox Added

--- src/CreateMessageFrame.java
+++ src/CreateMessageFrame.java
@@ -12,7 +12,7 @@ public class CreateMessageFrame extends JFrame {
     private JLabel toLabel;
     private JLabel msgLabel;
     private JTextField numberTextField;
-    private JTextField messageTextField;
+    private JTextArea messageTextArea;
     private JButton sendButton;
     private JButton cancelButton;
     CreateMessageFrame() {
@@ -30,6 +30,41 @@ public class CreateMessageFrame extends JFrame {
 
         contentPanel = new JPanel();
         contentPanel.setLayout(null);
+        add(contentPanel);
+        toLabel = new JLabel("TO:");
+        contentPanel.add(toLabel);
+        toLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        toLabel.setBounds(120, 10, 60, 10);
+        msgLabel = new JLabel("Message:");
+        contentPanel.add(msgLabel);
+        msgLabel.setHorizontalAlignment(SwingConstants.CENTER);
+        msgLabel.setBounds(80, 60, 140, 10);
+        numberTextField = new JTextField();
+        contentPanel.add(numberTextField);
+        numberTextField.setBounds(40, 30, 220, 20);
+        messageTextArea = new JTextArea();
+        //messageTextField.setBorder(BorderFactory.createLineBorder(Color.GRAY));
+        JScrollPane sp = new JScrollPane(messageTextArea);
+        sp.setBounds(40, 80, 220, 85);
+        contentPanel.add(sp);
+        sendButton = new JButton("Send");
+        sendButton.addActionListener(e -> {
 
+        });
+        sendButton.setBounds(40, 175, 80, 30);
+        contentPanel.add(sendButton);
+        cancelButton = new JButton("Cancel");
+        cancelButton.addActionListener(e -> {
+            setVisible(false);
+            dispose();
+        });
+        cancelButton.setBounds(180, 175, 80, 30);
+        contentPanel.add(cancelButton);
+
+        setTitle("Send Message");
+        setSize(300, 255);
+        setResizable(false);
+        setLocation(533, 210);
+        setVisible(true);
     }
 }
